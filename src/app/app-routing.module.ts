d8abb40dee4d403319ee262f5e8c79bd33d10c81
Rich Tillis <rich.tillis@learninga-z.com>
2021-05-03 15:26:38 -0700

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './admin/add/add.component';
+import { AdminComponent } from './admin/admin.component';
+import { EditComponent } from './admin/edit/edit.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin', component: AdminComponent,
+    children: [
+      {
+        path: "add",
+        component: AddComponent,
+      },
+      {
+        path: "edit",
+        component: EditComponent
+      }
+    ]
+  },
+  {
+    path: 'home', component: HomeComponent,
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './admin/add/add.component';
+import { EditComponent } from './admin/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
